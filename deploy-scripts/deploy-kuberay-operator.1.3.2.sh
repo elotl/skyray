c856545161a0d38f15af33f2c31c87205c80faa6
Anne Holler <anne@vmware.com>
2025-06-28 09:51:23 -0700

    update new kuberay-operator deployment, update cluster priority policy

--- deploy-scripts/deploy-kuberay-operator.1.3.2.sh
+++ deploy-scripts/deploy-kuberay-operator.1.3.2.sh
@@ -1,7 +1,6 @@
 helm install kuberay-operator kuberay/kuberay-operator --version 1.3.2 \
- --set 'env[0].value="true"' --set 'env[0].name=ENABLE_RAY_HEAD_CLUSTER_IP_SERVICE' \
+ --set-json 'env=[{"name":"ENABLE_RAY_HEAD_CLUSTER_IP_SERVICE","value":"true"}]' \
  $1
 kubectl label crd rayclusters.ray.io app.kubernetes.io/component=kuberay-operator
 kubectl label crd rayjobs.ray.io app.kubernetes.io/component=kuberay-operator
 kubectl label crd rayservices.ray.io app.kubernetes.io/component=kuberay-operator
-
