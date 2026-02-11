1b8e119c0b14925cf0ac46887a688c3702cc16a8
Anne Holler <anne@vmware.com>
2026-02-11 12:40:34 -0800

    Updates for NIMService --multi-cluster-capacity placement

--- deploy-scripts/deploy-nimservice.sh
+++ deploy-scripts/deploy-nimservice.sh
@@ -1,2 +1,2 @@
-kubectl apply -f ${SKYRAY_PATH}/deploy-scripts/nim-service.sample.yaml -n ${APP_NAMESPACE}
+envsubst < ${SKYRAY_PATH}/deploy-scripts/nim-service.sample.yaml | kubectl apply -n ${APP_NAMESPACE} -f -
 kubectl label nimservices.apps.nvidia.com/phi-3-mini-4k-instruct -n ${APP_NAMESPACE} app.kubernetes.io/instance=nimservice
