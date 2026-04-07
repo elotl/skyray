1e6875fc0c65086d0127a6dba93d42cc06175838
Anne Holler <anne@vmware.com>
2026-04-07 09:34:26 -0700

    Add files supporting demo of Nova lift and shift

---
+++ deploy-scripts/deploy-kuberay-label-crds.sh
@@ -0,0 +1,3 @@
+kubectl label crd rayclusters.ray.io app.kubernetes.io/component=kuberay-operator
+kubectl label crd rayjobs.ray.io app.kubernetes.io/component=kuberay-operator
+kubectl label crd rayservices.ray.io app.kubernetes.io/component=kuberay-operator
